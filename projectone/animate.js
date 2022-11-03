6edc18f343d4b08b85e5e4cce970a4d6c2901144
krstnayou <95864385+krstnayou@users.noreply.github.com>
2022-11-03 17:37:00 -0400

    Add files via upload

---
+++ projectone/animate.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+ $("#love").hover(function(){
+$("#heart").show()});
+ $("#work").hover(function(){
+$("#money").show()});
+ $("#addiction").hover(function(){
+$("#alcohol").show()});
+ $("#life").hover(function(){
+$("#other").show()});
+
+
+});
